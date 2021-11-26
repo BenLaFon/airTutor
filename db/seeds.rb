34db8afa7f1fbfd05d9879ffa7a3305b7d559353
Ben LaFon <benjamin.lafon@outlook.com>
2021-11-26 15:59:39 +0800

    yes seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,27 +9,27 @@ Booking.delete_all
 Student.delete_all
 Teacher.delete_all
 
-tester = User.create(name:' Roger McTeacher', role: 'teacher', phone_number: '158-666-6666', email: 'Roger.Mcteacher@gmail.com')
-teacher = Teacher.create(credential: "all them degrees", course_info: "You gon' learn today", education_background: "Like the best school ever", teaching_credentials:"my mom says im special", course_title: "THE BEST COURSE")
+tester = User.create(name:'Xun', role: 'teacher', phone_number: '158-666-6666', email: 'Amy@gmail.com')
+teacher = Teacher.create(credential: "Bootstrap Expert", course_info: "How to take care of cats", education_background: "Like all of the coding things", teaching_credentials:"my cats love me", course_title: "Ruby noob to Hero")
 tester.teacher = teacher
-tester1 = User.create(name:'Ms. Samson', role: 'teacher', phone_number: '158-888-8888', email: 'Samson.Ms@haircut.com')
-teacher1 = Teacher.create(credential: "I taught Gary", course_info: "1st grade counting, no potty breaks, no hitting", education_background: "Probably Highschool", teaching_credentials:"Gary says 'Very nice lady from back in the day'", course_title: "1st grade maths")
+tester1 = User.create(name:'Pavel', role: 'teacher', phone_number: '158-888-8888', email: 'Pavel@lewagon.com')
+teacher1 = Teacher.create(credential: "CSS Enthusiast", course_info: "All of the CSS info you will ever need", education_background: "Lewagon Alumni Maybe?", teaching_credentials:"Im like really good at math", course_title: "Pizza Vs. Realestate")
 tester1.teacher = teacher1
-tester2 = User.create(name:'Snoop Dog', role: 'teacher', phone_number: '420', email: 'snoopy_dogg@gmail.com')
-teacher2 = Teacher.create(credential: "None of your business", course_info: "Straight outta compton", education_background: "The Streets", teaching_credentials:"Platinum rapper", course_title: "Snoops Baking Bonanza")
+tester2 = User.create(name:'Aggy', role: 'teacher', phone_number: '1-800-8888', email: 'Ruby4Life@gmail.com')
+teacher2 = Teacher.create(credential: "None of your business", course_info: "Pushups and coding", education_background: "Also Lewagon probably", teaching_credentials:"None of your Business", course_title: "Databases and other things")
 tester2.teacher = teacher2
 
 
-terrence = User.create(name:'Terrence', role: 'student', phone_number: '12222323', email: 'terrence@terrence.com')
+terrence = User.create(name:'Terrence', role: 'student', phone_number: '158-244-4365', email: 'terrence@terrence.com')
 student = Student.create(age: 13)
 terrence.student = student
-terrence1 = User.create(name:'Gary', role: 'student', phone_number: '12222111111111323', email: 'gary@gary.com')
-student1 = Student.create(age: 111)
+terrence1 = User.create(name:'Gary', role: 'student', phone_number: '159-345-4531', email: 'gary@gary.com')
+student1 = Student.create(age: 29)
 terrence1.student = student1
-terrence2 = User.create(name:'Cuiwei', role: 'student', phone_number: '122222222222323', email: 'cuiwei@cuiwei.com')
-student2 = Student.create(age: 22)
+terrence2 = User.create(name:'Cuiwei', role: 'student', phone_number: '127-329-2234', email: 'cuiwei@cuiwei.com')
+student2 = Student.create(age: 20)
 terrence2.student = student2
 
-Booking.create(comment: "this is booking 1", student: student, teacher: teacher, status: 'accepted')
-Booking.create(comment: "this is booking 2", student: student1, teacher: teacher1, status: 'pending')
-Booking.create(comment: "this is booking 3", student: student2, teacher: teacher2, status: 'rejected')
+Booking.create(comment: "Lets learn Ruby", student: student, teacher: teacher, status: 'accepted', time: '9:00')
+Booking.create(comment: "CSS sounds Fun", student: student1, teacher: teacher1, status: 'pending', time: '10:00')
+Booking.create(comment: "No pushups please", student: student2, teacher: teacher2, status: 'pending', time: '11:00')
